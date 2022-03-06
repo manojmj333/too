6000e52d6545d4ceea0b162d875dd18b5b038edf
manojmj333 <96007736+manojmj333@users.noreply.github.com>
2022-03-06 19:11:15 +0530

    Update app.js
    added html

--- app.js
+++ app.js
@@ -23,11 +23,28 @@ app.post("/",function(req,res){
     const icon= weatherData.weather[0].icon;
     const imageURL= "http://openweathermap.org/img/wn/"+icon+"@2x.png"
 
-    res.write("<html><h3> The weather is "+ des+ "</h3></html>")
-    res.write("<h1> The temperature in " + cityname+" is "+ temp + " degree celsius"+"</h1>")
-    res.write("<img src="+imageURL+">")
+//     res.write("<html><h3> The weather is "+ des+ "</h3></html>")
+//     res.write("<h1> The temperature in " + cityname+" is "+ temp + " degree celsius"+"</h1>")
+//     res.write("<img src="+imageURL+">")
 
-    res.send();
+    res.send(`<!doctype html>
+          <html lang="en">
+            <head>
+              <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+              <meta name="description" content="">
+              <meta name="author" content="">
+              <title>WeatherNow - Check the current Weather in your City</title>
+              <!-- Bootstrap core CSS -->
+              <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+              <!-- Custom styles for this template -->
+              <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
+              <link href="css/style.css" rel="stylesheet">
+            </head>
+            <body>
+            <h3> The weather is ${des}</h3>
+            <img src="${imageURL}"/>
+            </body></html>`);
   })
   
   });
